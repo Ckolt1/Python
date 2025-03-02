02552164db4e5094ee7d4c18f237f8c06a47a3b2
Ckolt1 <cstevens337@students.mchenry.edu>
2025-03-02 14:50:58 -0600

    mpg

---
+++ Comments.py
@@ -0,0 +1,11 @@
+tank_size = 15
+# Define the total distance covered
+# TODO: add more tank sizes
+distance = 600
+# Calculate miles per gallon
+# How far the car can travel
+mpg = distance / tank_size  # formula for finding MPG
+
+# Print the result
+print("Miles per gallon:", mpg)
+# output of cars fuel efficiency
