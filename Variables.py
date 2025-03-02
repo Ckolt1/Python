3cdc40ee1bc27c0fbabffcc116230c334d3a9c90
Ckolt1 <cstevens337@students.mchenry.edu>
2025-03-02 14:46:03 -0600

    vars

---
+++ Variables.py
@@ -0,0 +1,15 @@
+kg_1 = 1
+kg_2 = 2
+kg_3 = 3
+kg_4 = 4
+# conversion factor of 2.20462
+p1 = kg_1 * 2.20462
+p2 = kg_2 * 2.20462
+p3 = kg_3 * 2.20462
+p4 = kg_4 * 2.20462
+
+print(f"{kg_1} kilograms is equal to {p1:.3f} pounds")
+print(f"{kg_2} kilograms is equal to {p2:.3f} pounds")
+print(f"{kg_3} kilograms is equal to {p3:.3f} pounds")
+print(f"{kg_4} kilograms is equal to {p4:.3f} pounds")
+print("All answers rounded to the hundredth place")
