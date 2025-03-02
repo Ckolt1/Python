19c70817421d074f3b92b39dc35de421255d779d
Ckolt1 <cstevens337@students.mchenry.edu>
2025-03-02 14:13:00 -0600

    datatypes

---
+++ Data_types.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 100000)
+
+# Integer
+y = 35
+print("Integer:", y)
+
+# Float
+z = 95.5
+print("Float:", z)
+
+# String
+name = "Jack"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
