57a07307d66fc0424a8e37ad0b9972a8395fca9c
Ckolt1 <cstevens337@students.mchenry.edu>
2025-03-09 14:04:51 -0500

    forloops

---
+++ forloops.py
@@ -0,0 +1,6 @@
+# if it wanted me to go up 300 then I think I needed to do 301 since the range function stops but does not include the end value?
+for num in range(1, 301):
+    # what its divisible  by (7)
+    if num % 7 == 0:
+
+        print(num)
