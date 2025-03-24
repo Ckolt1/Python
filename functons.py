6fa058af5267894bca79705d3384d775f5f3d6c1
Ckolt1 <cstevens337@students.mchenry.edu>
2025-03-23 21:50:45 -0500

    functions

---
+++ functons.py
@@ -0,0 +1,8 @@
+def square(side):
+      area = side * side
+      print(f"The square has an area of {area} square units.")
+square(55)
+def circle(radius):
+      area = radius * radius * 3.14
+      print (f"the circle has an area of {area} square units")
+circle(35)
