f91ccfe6ecd29522602b77d00fd64fefb659eeb7
Ckolt1 <cstevens337@students.mchenry.edu>
2025-04-06 18:11:40 -0500

    tryexcept

---
+++ exceptions.py
@@ -0,0 +1,10 @@
+# starting code gives error is you use letters instead of numbers
+
+def square_number():
+    try:
+        number = input("Enter a number to square: ")
+        squared_number = int(number) ** 2
+        print(f"The square of {number} is {squared_number}.")
+    except ValueError:
+        print("Error, Use valid NUMBERS")
+square_number()
