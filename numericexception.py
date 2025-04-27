e40fbd22e3cea33ee137bd9cbc28dd65f2cb2a69
Ckolt1 <cstevens337@students.mchenry.edu>
2025-04-27 15:48:20 -0500

    numerics

---
+++ numericexception.py
@@ -0,0 +1,32 @@
+class NotNumericError(Exception):
+    def __init__(self, message):
+
+        self.message = message
+
+        super().__init__(self.message)
+
+def get_numeric_input():
+
+    try:
+        user_input = input("Please enter a number: ")
+
+        if not user_input.isnumeric():
+
+            raise NotNumericError("Error, Input must be a numeric value.")
+        
+    except NotNumericError:
+
+        print("Error, Input must be a numeric value.")
+    else:
+
+        print("You entered a valid numeric value:", user_input)
+        return True  
+    finally:
+
+        print("End of Code.\n")
+
+# repeats until true
+while True:
+    
+    if get_numeric_input():
+        break
